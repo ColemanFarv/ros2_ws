aa352753d24692ebb587d320ba109a27b64190bd
Coleman <chrfarvolden@gmail.com>
2024-06-28 00:40:58 -0400

    New spectrometer working moveit and gazebo

--- build/mycobot_280/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/coleman/ros2_ws/install/mycobot_description/share/mycobot_description/package.sh"
-. "/home/coleman/ros2_ws/install/mycobot_interfaces/share/mycobot_interfaces/package.sh"
--- build/mycobot_280/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/coleman/ros2_ws/install/mycobot_280'
